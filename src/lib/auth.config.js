50047dd81486116440c8cd14fade5d039abe3b71
@Jatin-Kumar-Thakur <63.jatin@gmail.com>
2024-02-02 19:58:08 +0530

    Fix some bugs

--- src/lib/auth.config.js
+++ src/lib/auth.config.js
@@ -4,13 +4,22 @@ export const authConfig = {
     },
     providers: [],
     callbacks: {
-        async jwt({ token, user }) {
+        async jwt({ token, profile,account,user }) {
+            console.log(account);
             if (user) {
                 token.id = user.id;
                 token.isAdmin = user.isAdmin;
             }
             return token;
         },
+        // async jwt({ token, account, user }) {
+        //     // Persist the OAuth access_token and or the user id to the token right after signin
+        //     if (account) {
+        //         // token.accessToken = account.access_token
+        //         token.id = user.id
+        //     }
+        //     return token
+        // },
         async session({ session, token }) {
             if (token) {
                 session.user.id = token.id;
@@ -18,16 +27,17 @@ export const authConfig = {
             }
             return session;
         },
-        authorized({auth, request }) {
-            // console.log(auth.user);
+        authorized({ auth, request }) {
             const userD = auth?.user;
+            // console.log();
+
 
             // console.log(auth)
             const isOnAdminPanel = request.nextUrl?.pathname.startsWith("/admin");
             const isOnBlogPage = request.nextUrl?.pathname.startsWith("/blog");
             const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login");
             const isOnUserPage = request.nextUrl?.pathname.startsWith("/aboutuser");
-            
+
 
             // ONLY ADMIN CAN REACH THE ADMIN DASHBOARD
             if (isOnAdminPanel && !userD?.isAdmin) {
@@ -44,9 +54,9 @@ export const authConfig = {
             if (isOnLoginPage && userD) {
                 return Response.redirect(new URL("/", request.nextUrl));
             }
-            
+
             // for about page
-            if(isOnUserPage && !userD){
+            if (isOnUserPage && !userD) {
                 return false;
             }
             return true
